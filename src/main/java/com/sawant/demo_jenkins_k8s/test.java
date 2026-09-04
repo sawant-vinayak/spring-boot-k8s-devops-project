ee5a32da36de7de1993380f93da4477a77c7c1fe
vsawantvinay <vsawant.azure@gmail.com>
2026-09-04 23:41:29 +0530

    initial commit

---
+++ src/main/java/com/sawant/demo_jenkins_k8s/test.java
@@ -0,0 +1,13 @@
+package com.sawant.demo_jenkins_k8s;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class test {
+    @GetMapping ("/Welcome")
+    public String Welcome() {
+        return "Welcome to my demo on devops";
+    }
+}
